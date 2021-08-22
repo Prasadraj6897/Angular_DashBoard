0a257b6959bc1ca35c42dc4010abf8bf81a531e6
Prasadraj6897 <prasadraj32895@gmail.com>
2021-08-22 17:58:37 +0530

    Imported Fullwidth compo in module.ts

--- src/app/layouts/full-width/full-width.module.ts
+++ src/app/layouts/full-width/full-width.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FullWidthModule
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FlexLayoutModule,
+    SharedModule
   ]
 })
 export class FullWidthModule { }
